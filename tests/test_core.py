9c2c2ffbf9d0d0a6c5eb864274ca4ca15280b50c
Ryan Nelson <rnelsonchem@gmail.com>
2022-10-01 10:28:30 -0600

    Move parameter input outside of class
    This makes it easier to use for several tests.

--- tests/test_core.py
+++ tests/test_core.py
@@ -13,24 +13,29 @@ from costcalc.constants import *
 # The data file directory
 ddir = Path(dirname(__file__), 'data')
 
+# Use Pandas to read in the test Excel files 
+# For the reactions sheets, the dtype must be specified for a couple of
+# columns
+mats = pd.read_excel(ddir / 'clean_mat.xlsx')
+br_clean = pd.read_excel(ddir / 'clean_rxn.xlsx', 
+        sheet_name='Bromine Route',
+        dtype={RXN_STP:str, RXN_CST:str})
+cl_clean = pd.read_excel(ddir / 'clean_rxn.xlsx', 
+        sheet_name='Chlorine Route',
+        dtype={RXN_STP:str, RXN_CST:str})
+
+
+### Tests ###
+
+@pytest.mark.parametrize(
+        "rxn, mats, cost",
+        [(br_clean, mats, 43.05580071473825),
+        (cl_clean, mats, 61.25477348169882),
+        ]
+)
 class Test_CoreFunctions(object):
-
-    @pytest.mark.parametrize(
-            "name, cost",
-            [('Bromine Route', 43.05580071473825),
-            ('Chlorine Route', 61.25477348169882),
-            ]
-    )
-    def test_calc_cost_clean(self, name, cost):
-        # Use Pandas to read in the test Excel files
-        # For the reactions sheets, the dtype must be specified for a couple of
-        # columns
-        mats = pd.read_excel(ddir / 'clean_mat.xlsx')
-        rxns = pd.read_excel(ddir / 'clean_rxn.xlsx',
-                sheet_name = name,
-                dtype={RXN_STP:str, RXN_CST:str})
-
-        coster = CoreCost(mats, rxns, 'Product')
+    def test_calc_cost_clean(self, rxn, mats, cost):
+        coster = CoreCost(mats, df, 'Product')
         coster.calc_cost()
         assert_allclose(coster.cost, cost)
 
