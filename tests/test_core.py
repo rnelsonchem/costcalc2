43bb916855ac6d9dfd2dc9a62f5f171bad15bae4
rnelsonchem <rnelsonchem@gmail.com>
2022-10-30 20:07:42 -0600

    Move data-reading code into a new module
    This will make sharing data-related stuff easier between modules.
    
    This required changing how pytest imports the tests, so I ended up making the tests folder into a "package".

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,3 @@
-from os.path import dirname
-from pathlib import Path
-
 import pytest
 import pandas as pd
 import numpy as np
@@ -12,65 +9,7 @@ from pandas.testing import *
 from costcalc import CoreCost
 from costcalc.constants import *
 from costcalc.exceptions import CostError
-
-# The data file directory
-ddir = Path(dirname(__file__), 'data')
-
-# Load the test data for comparisons
-# For some sheets, the dtype must be specified for string columns
-# Materials file
-mat_clean = pd.read_csv(ddir / 'clean_mat.csv')
-
-# Linear and convergent rxn sheets
-lin_clean = pd.read_csv(ddir / 'clean_lin_route.csv', 
-        dtype={RXN_STP:str, RXN_CST:str})
-con_clean = pd.read_csv(ddir / 'clean_conv_route.csv', 
-        dtype={RXN_STP:str, RXN_CST:str})
-
-# Linear and convergent fulldata DataFrames
-lin_clean_fd = pd.read_csv(ddir / 'clean_lin_fd.csv',
-        dtype={RXN_STP:str, RXN_CST:str})\
-                .set_index([RXN_STP, RXN_CPD])
-con_clean_fd = pd.read_csv(ddir / 'clean_conv_fd.csv',
-        dtype={RXN_STP:str, RXN_CST:str})\
-                .set_index([RXN_STP, RXN_CPD])
-
-# Linear and convergent compact results DataFrames
-lin_clean_res_c = pd.read_csv(ddir / 'clean_lin_res_com.csv',
-        dtype={RXN_STP:str, })\
-                .set_index([RXN_STP, RXN_CPD])
-con_clean_res_c = pd.read_csv(ddir / 'clean_con_res_com.csv',
-        dtype={RXN_STP:str, })\
-                .set_index([RXN_STP, RXN_CPD])
-
-# Linear and convergent full results DataFrames
-lin_clean_res_f = pd.read_csv(ddir / 'clean_lin_res_ful.csv',
-        dtype={RXN_STP:str, RXN_CST:str})\
-                .set_index([RXN_STP, RXN_CPD])
-con_clean_res_f = pd.read_csv(ddir / 'clean_con_res_ful.csv',
-        dtype={RXN_STP:str, RXN_CST:str})\
-                .set_index([RXN_STP, RXN_CPD])
-
-# Linear and convergent Excel DataFrames
-# The "Cost" column has multiple dtypes, that are not set correctly when the
-# data is imported via Pandas. This function fixes the dyptes
-def dtype_fix(x):
-    try:
-        return float(x)
-    except:
-        return x
-
-lin_clean_excel = pd.read_csv(ddir / 'clean_lin_excel.csv',
-        dtype={RXN_STP:str, RXN_CST:str})\
-                .set_index([RXN_STP, RXN_CPD])
-lin_clean_excel['Cost'] = lin_clean_excel['Cost']\
-        .apply(dtype_fix)
-con_clean_excel = pd.read_csv(ddir / 'clean_con_excel.csv',
-        dtype={RXN_STP:str, RXN_CST:str})\
-                .set_index([RXN_STP, RXN_CPD])
-con_clean_excel['Cost'] = con_clean_excel['Cost']\
-        .apply(dtype_fix)
-
+from .data_build import *
 
 ### Tests - Working reactions ###
 
