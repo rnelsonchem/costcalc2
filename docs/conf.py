5e73fb345ff185af0b13a0994cc935ba41ecaae7
rnelsonchem <rnelsonchem@gmail.com>
2023-11-07 17:21:56 -0700

    Version bump: 0.10.3
    Small fix for solvent checks.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Ryan Nelson'
 author = 'Ryan Nelson'
 
 # The full version, including alpha/beta/rc tags
-release = '0.10.2'
+release = '0.10.3'
 
 
 # -- General configuration ---------------------------------------------------
